b205e1aacb0d089618e91c88a12df876532c4d56
MariaB <lvlaria.15@gmail.com>
2023-10-24 12:32:37 +0300

    Корзина товаров доделана

--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./shop-header.css";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  bookAddedToCart,
+  bookRemovedFromCart,
+  allBooksRemovedFromCart,
+} from "../../actions";
 
-const ShopHeader = ({ numItems, total }) => {
+const ShopHeader = ({ items, total }) => {
+  let countItem = 0;
+  items.forEach((element) => {
+    countItem += element.count;
+  });
   return (
     <header className="shop-header">
       <Link to="/" className="logo text-dark">
@@ -12,11 +22,24 @@ const ShopHeader = ({ numItems, total }) => {
       <div className="shopping-cart">
         <Link to="/cart" className="link">
           <i className="cart-icon fa-solid fa-cart-shopping" />
-          {numItems} items (${total})
+          {countItem} items (${total})
         </Link>
       </div>
     </header>
   );
 };
 
-export default ShopHeader;
+const mapStateToProps = ({ cartItems, orderTotal }) => {
+  return {
+    items: cartItems,
+    total: orderTotal,
+  };
+};
+
+const mapDispatchToProps = {
+  onIncrease: bookAddedToCart,
+  onDecrease: bookRemovedFromCart,
+  onDelete: allBooksRemovedFromCart,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopHeader);
